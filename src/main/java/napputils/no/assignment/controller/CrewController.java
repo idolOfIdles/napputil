4dfddd358269b9de4206559bce83d99530afa686
safayat <safayat@cefalo.com>
2020-12-21 00:29:43 +0600

    return dto instead of db entity

--- src/main/java/napputils/no/assignment/controller/CrewController.java
+++ src/main/java/napputils/no/assignment/controller/CrewController.java
@@ -1,26 +1,39 @@
 package napputils.no.assignment.controller;
 
 import napputils.no.assignment.model.Crew;
+import napputils.no.assignment.model.dto.CrewDto;
+import napputils.no.assignment.model.dto.CrewLightDto;
 import napputils.no.assignment.service.CrewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/api")
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class CrewController {
 
     @Autowired
     private CrewService crewService;
 
     @GetMapping("/crew/{crewId}")
-    public Crew getCrew(@PathVariable String crewId){
-        return crewService.getCrew(crewId).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    public CrewDto getCrew(@PathVariable String crewId) {
+        return crewService.getCrew(crewId);
+    }
+
+    @PostMapping(path = "/crew")
+    public CrewDto insertCrew(@RequestBody Crew crew) {
+        return crewService.insert(crew);
+    }
+
+    @PutMapping("/crew")
+    public CrewDto update(@RequestBody Crew crew) {
+        return crewService.update(crew);
+    }
+
+    @GetMapping("/crew/list")
+    public List<CrewLightDto> list() {
+        return crewService.getCrewList();
     }
 
 }
--- src/main/java/napputils/no/assignment/service/CrewService.java
+++ src/main/java/napputils/no/assignment/service/CrewService.java
@@ -1,31 +1,38 @@
 package napputils.no.assignment.service;
 
 import napputils.no.assignment.model.Crew;
+import napputils.no.assignment.model.dto.CrewDto;
+import napputils.no.assignment.model.dto.CrewLightDto;
 import napputils.no.assignment.repository.CrewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CrewService {
     @Autowired
     public CrewRepository repository;
 
-    public Optional<Crew> getCrew(String id) {
-        return repository.findById(id);
+    public CrewDto getCrew(String id) {
+        Crew crew = repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return CrewDto.convert(crew);
     }
 
-    public List<Crew> getCrewList() {
-        return repository.findAll();
+    public List<CrewLightDto> getCrewList() {
+        return repository.findAll().stream().map(crew->CrewLightDto.convert(crew)).collect(Collectors.toList());
     }
 
-    public Crew insert(Crew crew) {
-        return repository.insert(crew);
+    public CrewDto insert(Crew crew) {
+        return CrewDto.convert(repository.insert(crew));
     }
 
-    public Crew update(Crew crew) {
-        return repository.save(crew);
+    public CrewDto update(Crew crew) {
+        return CrewDto.convert(repository.save(crew));
     }
 }
